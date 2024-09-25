045d8a169666df5c1771cbdeee8f0d26fa052c4f
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-25 20:19:30 +0500

    Updated App Code For Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/weather-app-using-apis/',  // replace with your repository name
   plugins: [react()],
 })
